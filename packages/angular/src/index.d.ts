b9e1cdb0fb09cadf4e48ecf7b8d2eef066f30c38
Shuoze Li <szsuzyli@gmail.com>
2026-06-07 16:35:38 +0000

    chore: ignore generated package outputs

--- packages/angular/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { CompilerTarget } from "@open-ui-ir/compiler-core";
-export declare const angularTarget: CompilerTarget;
-//# sourceMappingURL=index.d.ts.map
--- packages/react-antd/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { CompilerTarget } from "@open-ui-ir/compiler-core";
-export declare const reactAntdTarget: CompilerTarget;
-//# sourceMappingURL=index.d.ts.map
